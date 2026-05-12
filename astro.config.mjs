46053c2cf883352f6938967e193c632c501a5c41
dinhkien <kien.ha@chek.vn>
2026-05-12 13:28:27 +0700

    seo(meta): add sitemap and structured metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio.dinhkien.id.vn',
   output: "static",
 
   vite: {
     plugins: [tailwindcss()]
   },
+
+  integrations: [sitemap()],
 
   adapter: cloudflare(),
 });
